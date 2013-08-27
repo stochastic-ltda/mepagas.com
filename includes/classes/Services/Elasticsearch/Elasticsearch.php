de5ba6bba2479a475f76d1f0b16d20d3051b53d6
root <root@79-52-235-37.static.edis.at>
2013-08-26 23:39:26 -0400

    cambio de url elasticsearch

--- includes/classes/Services/Elasticsearch/Elasticsearch.php
+++ includes/classes/Services/Elasticsearch/Elasticsearch.php
@@ -13,7 +13,7 @@ class Elasticsearch {
 	function __construct($index=null){
 
 		if(!is_null($index)) {
-			$this->_client = new \Elastica\Client( array('host' => 'mepagas.dev','port' => 9200) );
+			$this->_client = new \Elastica\Client( array('host' => 'mepagas.com','port' => 9200) );
 			$this->_index = $this->_client->getIndex($index);
 		}
 	}
