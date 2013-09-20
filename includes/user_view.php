2730aaaa9e57acc7664245fe72c5fadbc329c3f4
root <pqzada@gmail.com>
2013-09-20 12:38:46 -0300

    fix problema en numero avisos

--- includes/user_view.php
+++ includes/user_view.php
@@ -47,6 +47,7 @@ $.post('/includes/phpscripts/user_is_user.php', {code: getCookie('mutm_gif'), us
 		<div id="user_avisos">
 			<h3>Mis Avisos</h3>
 			<table width="100%" cellpadding="5" cellspacing="0">
+			<? if(!is_null($aviso) && count($aviso) > 0): ?>
 			<? foreach($avisos as $aviso): ?>
 				<tr class="aviso-row">
 					<td><a href="/<?=$aviso->get('id')?>/<?=$aviso->get('permalink')?>" target="_blank"><?=$aviso->get('tipo')?> <?=$aviso->get('precio')?> y <?=$aviso->get('titulo')?></a></td>
@@ -58,6 +59,7 @@ $.post('/includes/phpscripts/user_is_user.php', {code: getCookie('mutm_gif'), us
 					</td>
 				</tr>
 			<? endforeach; ?>
+			<? endif; ?>
 			</table>
 		</div>
 
