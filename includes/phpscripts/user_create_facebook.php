d5ceb37f9fb4d0484686a78178603e7a117c1c22
root <pqzada@gmail.com>
2013-10-04 16:16:04 -0300

    se elimina constante actualizacion de facebook y se permite editar perfiles de face

--- includes/phpscripts/user_create_facebook.php
+++ includes/phpscripts/user_create_facebook.php
@@ -15,12 +15,14 @@ if($usuarioMapper->findEmail($_POST['email'])) {
 	// Actualizo informacion de usuario
 	$usuarioOLD = $usuarioMapper->findByEmail($_POST['email']);
 	$userID = $usuarioOLD->get('id');
+	/*
 	$usuario->set('nombre_empresa', $usuarioOLD->get('nombre_empresa'));
 	$usuario->set('acercade', $usuarioOLD->get('acercade'));
 	$usuario->set('telefono', $usuarioOLD->get('telefono'));
 	$usuario->set('token', $usuarioOLD->get('token'));
 	$usuario->set('estado', $usuarioOLD->get('estado'));
 	$usuarioMapper->update($usuarioOLD->get('id'), $usuario);
+	*/
 
 } else {
 	
